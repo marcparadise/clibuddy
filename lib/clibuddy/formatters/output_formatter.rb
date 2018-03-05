eb1e2db9d2eb1a7b18ab71891de59c2175c60d41
Marc A. Paradise <marc.paradise@gmail.com>
2018-03-05 13:35:20 -0500

    Add hack to find tokens surrounded by [], and remove debug code

--- lib/clibuddy/formatters/output_formatter.rb
+++ lib/clibuddy/formatters/output_formatter.rb
@@ -5,7 +5,7 @@ module CLIBuddy
     class OutputFormatter
       TAB_ESCAPE = /\.t /
       NEWLINE_ESCAPE = /\.n$/
-      ENV_MARKER = /(.*)\.e\s(\w+)(.*)/
+      ENV_MARKER = /(.*)\.e\s([a-zA-Z_]+)(.*)/
 
       def self.format(msg, mapped_args)
         Array(msg).collect do |line|
@@ -14,14 +14,19 @@ module CLIBuddy
       end
 
       def self.format_line(line, mapped_args)
-        #puts ">>>> RAW: #{line}"
         pastel = Pastel.new
         line.split(" ").each do |token|
+          # TODO - Crappy hack in here - knowing that we often
+          # put tokens inside of [ ] , we explicitly also try to match
+          # on removed first/last char
+          if token =~ /\[(.*)\]/
+            token = $1
+          end
+
           if mapped_args.has_key? token
             line.gsub!(/(?<!\/e)#{token}/, pastel.green(mapped_args[token]))
           end
         end
-        #puts ">>>> p2: #{line}"
         line.gsub!(NEWLINE_ESCAPE, "\n")
         line.gsub!(TAB_ESCAPE, "    ")
         # Escaped /PARAM_NAME should be rendered without the escape
@@ -44,11 +49,9 @@ module CLIBuddy
           # $1 - text prefix
           # $2 - env var name
           # $3 - trailing text
-          val = ENV[$2] || pastel.red("${$2}")
+          val = ENV[$2] || pastel.red("#{$2}")
           line = "#{$1}#{val}#{$3}"
         end
-        #puts ">>> FORMATTED: #{line}"
-
         line
       end
     end
